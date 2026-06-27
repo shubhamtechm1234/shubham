ed0d2fef8a64204f34d41fadfa26a8fa5505d33c
shubhamtechm1234 <91341977+shubhamtechm1234@users.noreply.github.com>
2026-06-27 13:18:37 +0530

    Update health.sh

--- health.sh
+++ health.sh
@@ -1,36 +1,27 @@
 #!/bin/bash
+for service in jenkins httpd; do pgrep -f "$service" >/dev/null;
+if [ $? -eq 0 ];
+then
+echo "$service is running"
+ps -C httpd -o %cpu,%mem --no-headers | \
+awk '{cpu+=$1; mem+=$2} END {print "CPU:",cpu"%","MEM:",mem"%"}'
+else
+echo "$service is not running"
+sudo systemctl status $service | awk 'NR==3{print $1,$2}'
+echo "Starting the service of this $service"
+sudo systemctl start $service
+sleep 5
 
-for service in jenkins httpd; do
-
-    if pgrep -f "$service" >/dev/null; then
-        echo "$service is running"
-
-        ps -C httpd -o %cpu,%mem --no-headers | \
-        awk '{cpu+=$1; mem+=$2}
-             END {print "CPU:",cpu"%","MEM:",mem"%"}'
-
-    else
-        echo "$service is not running"
-
-        sudo systemctl status "$service" | awk 'NR==3{print $1,$2}'
-
-        echo "Starting the service of $service"
-        sudo systemctl start "$service"
-
-        sleep 5
-
-        if pgrep -f "$service" >/dev/null; then
-            echo "$service is running now"
-
-            sudo systemctl status "$service" | awk 'NR==3{print $1,$2}'
-
-            ps -C httpd -o %cpu,%mem --no-headers | \
-            awk '{cpu+=$1; mem+=$2}
-                 END {print "CPU:",cpu"%","MEM:",mem"%"}'
-        else
-            echo "We got an error while running $service"
-        fi
-    fi
-
+pgrep -f $service >/dev/null
+if [ $? -eq 0 ];
+then
+echo "$service is running now"
+sudo systemctl status $service | awk 'NR==3{print $1,$2}'
+ps -C httpd -o %cpu,%mem --no-headers | \
+awk '{cpu+=$1; mem+=$2} END {print "CPU:",cpu"%","MEM:",mem"%"}'
+else
+echo "we got error while runing the $service"
+fi
+fi
 done
 
