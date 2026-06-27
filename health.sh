25637a7cc23d2a2ce0d61b18231a6763a4f27f5a
shubhamtechm1234 <91341977+shubhamtechm1234@users.noreply.github.com>
2026-06-27 13:56:09 +0530

    Update health.sh

--- health.sh
+++ health.sh
@@ -3,11 +3,11 @@ for service in jenkins httpd; do pgrep -f "$service" >/dev/null;
 status=$?
 if [ $status -eq 0 ];
 then
-echo "$service is RUNNING"
+echo "$service is runing"
 ps -C httpd -o %cpu,%mem --no-headers | \
 awk '{cpu+=$1; mem+=$2} END {print "CPU:",cpu"%","MEM:",mem"%"}'
 else
-echo "$service is not RUNNING"
+echo "$service is not runing"
 sudo systemctl status $service | awk 'NR==3{print $1,$2}'
 echo "Starting the service of this $service"
 sudo systemctl start $service
