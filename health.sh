4d47c1163b2f21627c89688046b3ebeda030fa6a
shubhamtechm1234 <91341977+shubhamtechm1234@users.noreply.github.com>
2026-07-07 13:01:03 +0530

    Update health.sh

--- health.sh
+++ health.sh
@@ -17,7 +17,7 @@ pgrep -f $service >/dev/null
 status=$?
 if [ $status -eq 0 ];
 then
-echo "$service is running now"
+echo "$service is RUNNING NOW"
 sudo systemctl status $service | awk 'NR==3{print $1,$2}'
 ps -C httpd -o %cpu,%mem --no-headers | \
 awk '{cpu+=$1; mem+=$2} END {print "CPU:",cpu"%","MEM:",mem"%"}'
